ad678e45e8463f053cf0643c2a71beb13288ab04
greg <gcharnac@yahoo.com>
2018-01-24 18:53:36 +0100

    dataset

---
+++ extract_metadata.py
@@ -0,0 +1,32 @@
+# Data Enrichment
+
+# trying to join "reviews_data" table on "product_metadata" table to get:
+#  - product title 
+#  - product price 
+#  - product brand
+
+# source code from: http://jmcauley.ucsd.edu/data/amazon/links.html (Reading the data - Pandas data frame)
+
+import pandas as pd
+import gzip
+
+def parse(path):
+  g = gzip.open(path, 'rb')
+  for l in g:
+    yield eval(l)
+
+def getDF(path):
+  i = 0
+  df = {}
+  for d in parse(path):
+    df[i] = d
+    i += 1
+  return pd.DataFrame.from_dict(df, orient='index')
+
+df = getDF('data/reviews_Clothing_Shoes_and_Jewelry_5.json.gz')
+
+light_df = df.loc[:,['asin', 'reviewerID', 'reviewerName','reviewText']]
+
+light_df = light_df.rename(index=str, columns={"rewiewerID": "customerID", "reviewerName": "customerName",'reviewText':'Text'})
+
+light_df.to_csv('data/data_transformer.csv', sep=',')
